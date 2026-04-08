e8b4eba96d3cfe591ec645623a753fb9b1056b6a
zouzouflorent <florent.zouzou@epitech.eu>
2026-04-08 09:38:10 +0000

    /Init backend

--- backend/src/vital-events/vital-events.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { VitalEventsController } from './vital-events.controller';
-import { VitalEventsService } from './vital-events.service';
-
-@Module({
-  controllers: [VitalEventsController],
-  providers: [VitalEventsService],
-})
-export class VitalEventsModule {}
