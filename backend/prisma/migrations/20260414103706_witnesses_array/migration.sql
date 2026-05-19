0626c7c1eb690541e8ba30d87aa21fb0df21c864
zouzouflorent <florent.zouzou@epitech.eu>
2026-05-19 17:51:20 +0000

    chore: remove backend/ and mobile/ from GitHub tracking (kept local only)

--- backend/prisma/migrations/20260414103706_witnesses_array/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `witness_name` on the `marriage_records` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "marriage_records" DROP COLUMN "witness_name",
-ADD COLUMN     "witnesses" JSONB DEFAULT '[]';
